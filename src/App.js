271004a3f64fd4bf24faaf240f1de03817875eda
DeanYoon <68269605+DeanYoon@users.noreply.github.com>
2023-01-26 21:36:02 +0900

    before typescript

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from "styled-components";
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const rotateAnimation = keyframes`
+0%{
+transform: rotate(0deg);
+border-radius: 0px;
+
+}50%{
+transform: rotate(180deg);
+border-radius: 100px;
+
+}100%{
+  transform: rotate(360deg);
+border-radius: 0px;
+
+}
+`;
+const Emoji = styled.span`
+  font-size: 100px;
+`;
+
+const Box = styled.div`
+  background-color: tomato;
+  height: 200px;
+  width: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  animation: ${rotateAnimation} 2s linear infinite;
+  ${Emoji}:hover {
+    font-size: 80px;
+  }
+`;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Box>
+        <Emoji>🥰</Emoji>
+      </Box>
+      <Emoji>😄</Emoji>
+    </Wrapper>
   );
 }
 
